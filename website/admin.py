8befb20d7c993df7b041b216f20fc9e75b81dd37
Emmil Joshua Caguiat <ejt.caguiat@gmail.com>
2020-08-18 14:33:06 -0400

    Import_Export Setup
    changed the settings.py and models.py to make sure excel files can be directly imported to the model.

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+from import_export.admin import ImportExportModelAdmin
+
+@admin.register(Case)
+class ViewAdmin(ImportExportModelAdmin):
+    pass
--- website/models.py
+++ website/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Cases(models.Model):
+class Case(models.Model):
     date = models.CharField(max_length = 100, blank = False)
+    positive = models.IntegerField() #Positive Covid cases
+    negative = models.IntegerField() #Negative Covid cases
+    recovered = models.IntegerField() #Recovered Covid cases
+    total = models.IntegerField() #Total Covid Tests done
+    positiveIncrease = models.IntegerField() #Increase of Positive Covid cases per day
+    negativeIncrease = models.IntegerField() #Increase of Negative Covid cases per day
+    deathIncrease = models.IntegerField() #Increase of Covid deaths per day
